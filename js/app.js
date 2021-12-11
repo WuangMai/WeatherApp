a315a92a6b9667492d77701899709002f8bce3d0
wuang <wuangmai@gmail.com>
2021-12-11 10:55:41 +0100

    cleanup, basic version with static city set, current weather and 3 days forecast

--- js/app.js
+++ js/app.js
@@ -1,15 +1,13 @@
 let apiKey = "a014b0e2a0ce4843bb174047212908";
-let city = "auto:ip";
-let apiURL = `http://api.weatherapi.com/v1/forecast.json?key=${apiKey}&q=${city}&days=5`;
+let city = "Wroclaw";
+let apiURL = `http://api.weatherapi.com/v1/forecast.json?key=${apiKey}&q=${city}&days=5&aqi=no&alerts=no`;
 
 async function now() {
     let data = await fetch(apiURL).then(res => res.json());
     let {location, current} = data;
-
-    let img = current.condition.text.toLowerCase().replace(" ", "-");
-    let now = new Date().getHours();
+    // console.log(current);
     document.querySelector(".weather__icon img")
-        .setAttribute("src", `assets/icons/${img}${now > 6 && now < 18 ? "-day" : "-night"}.svg`);
+        .setAttribute("src", current.condition.icon);
 
     document.querySelector(".city__name").innerText = location.name;
     document.querySelector(".temperature__value").innerText = current.temp_c;
@@ -18,32 +16,24 @@ async function now() {
     document.querySelector(".wind-speed__value").innerText = ` ${current.wind_kph} km/h`;
 }
 
-async function forecast(){
+async function forecast() {
     let data = await fetch(apiURL).then(res => res.json());
-    let {forecast} = data;
     let ul = document.querySelector(".weather__forecast");
-    let newLi = document.createElement("li");
 
-    let spanDay = document.createElement("span");
-    spanDay.classList.add("day");
-    let date = forecast.forecastday[0].date;
-    let weekday = new Date(date);
-    spanDay.innerText = weekday.toLocaleDateString("pl-PL",{weekday: 'long'});
-
-
-
-    let img = document.createElement("img");
-    let spanTemp = document.createElement("span");
-
-
-    spanTemp.classList.add("temperature");
-    spanTemp.classList.add("temperature__value");
-
-    newLi.appendChild(spanDay).appendChild(img).appendChild(spanTemp);
-    ul.appendChild(newLi);
-
-
-    console.log(forecast);
+    ul.innerHTML = "";
+    data.forecast.forecastday.forEach(el => {
+        const days = ["Niedziela", "Poniedziałek", "Wtorek", "Środa", "Czwartek", "Piątek", "Sobota"]
+        let dayOfWeek = new Date(el.date);
+        ul.innerHTML += (
+            `<li>
+                <span class="day">${days[dayOfWeek.getDay()]}</span>
+                <img src="${el.day.condition.icon}"/>
+                <span class="temperature">                    
+                <span class="temperature__value">${el.day.maxtemp_c}</span>&deg;C
+                </span>
+            </li>`
+        )
+    });
 }
 
 now();
